eb1d86d3595033f15ec2911c76946b1f88f816c6
hashrock <hashrock@users.noreply.github.com>
2017-06-27 00:49:03 +0900

    whoooooo

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ function tategaki(ctx, text, x, y, px) {
   }
 }
 
+function drawTextLines(ctx, text, x, y, fontsize) {
+  var lines = text.split("\n");
+  var sx = x + fontsize
+  var sy = y
+  var maxlen = 0
+  for (var l = 0; l < lines.length; l++) {
+    var line = lines[l]
+    tategaki(ctx, line, x - fontsize * l, y, fontsize);
+    maxlen = maxlen < line.length ? line.length : maxlen
+  }
+  ctx.strokeRect(sx, sy, -fontsize * lines.length, fontsize * maxlen)
+}
+
 
 
 new Vue({
@@ -35,33 +48,24 @@ new Vue({
 
       var ctx = this.ctx
 
-      for(var i = 0; i < 4; i++){
+      for (var i = 0; i < 4; i++) {
         var ox = 20
         var oy = 20 + (280 + 20) * i
-        ctx.strokeStyle="black"
-        ctx.lineWidth=4
+        ctx.strokeStyle = "black"
+        ctx.lineWidth = 4
         ctx.strokeRect(ox, oy, 420, 280)
 
         var fontsize = 18;
         var x
         var y = oy + 30
-        if(j[i].length > 0){
+        if (j[i].length > 0) {
           x = ox + 400 - fontsize
-          var lines = j[i][0].split("\n");
-          var sx = x + fontsize
-          var sy = y
-          var maxlen = 0
-          for(var l = 0; l < lines.length; l++){
-            var line = lines[l]
-            tategaki(ctx, line, x - fontsize * l, y, fontsize);
-            maxlen = maxlen < line.length ? line.length : maxlen
-          }
-          ctx.strokeRect(sx, sy, -fontsize * lines.length, fontsize * maxlen)
+          drawTextLines(ctx, j[i][0], x, y, fontsize)
         }
-        if(j[i].length > 1){
-          x = ox + 30 - fontsize
-          ctx.strokeRect(x, y, fontsize, fontsize * j[i][1].length)
-          tategaki(ctx, j[i][1], x, y, fontsize);
+        if (j[i].length > 1) {
+          var linelength = j[i][1].split("\n").length;
+          x = ox + 20 + fontsize * (linelength - 1)
+          drawTextLines(ctx, j[i][1], x, y, fontsize)
         }
 
       }
@@ -71,7 +75,7 @@ new Vue({
   mounted: function () {
     var canvas = document.querySelector("#output")
     var w = 420 + 20 * 2
-    var h = (280 + 20) * 4 + 20 
+    var h = (280 + 20) * 4 + 20
 
     canvas.width = `${w}`
     canvas.height = `${h}`
