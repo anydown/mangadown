3d91961b9413815a079916dededa13f8bde16fbe
hashrock <hashrock@users.noreply.github.com>
2020-08-21 22:30:15 +0900

    refactor: to ES6

--- src/components/MangaDown.vue
+++ src/components/MangaDown.vue
@@ -147,7 +147,7 @@ function isFirefox() {
 function tategaki(ctx, text, x, y, px, font) {
   ctx.fillStyle = "";
   ctx.font = px + `px '${font}'`;
-  for (var i = 0; i < text.length; i++) {
+  for (let i = 0; i < text.length; i++) {
     ctx.save();
     ctx.translate(x, y + px * i);
     if (isFirefox()) {
@@ -162,16 +162,16 @@ function tategaki(ctx, text, x, y, px, font) {
 }
 
 function drawTextLines(ctx, text, x, y, fontsize, font) {
-  var lines = text.split("\n");
-  var sx = x + fontsize;
-  var sy = y;
-  var maxlen = 0;
-  for (var l = 0; l < lines.length; l++) {
-    var line = lines[l];
+  const lines = text.split("\n");
+  const sx = x + fontsize;
+  const sy = y;
+  let maxlen = 0;
+  for (let l = 0; l < lines.length; l++) {
+    const line = lines[l];
     maxlen = maxlen < line.length ? line.length : maxlen;
   }
-  var cx = sx - (fontsize * lines.length) / 2;
-  var cy = sy + (fontsize * maxlen) / 2;
+  const cx = sx - (fontsize * lines.length) / 2;
+  const cy = sy + (fontsize * maxlen) / 2;
 
   ctx.save();
   ctx.beginPath();
@@ -189,17 +189,17 @@ function drawTextLines(ctx, text, x, y, fontsize, font) {
   ctx.stroke();
   ctx.restore();
 
-  for (var l = 0; l < lines.length; l++) {
-    var line = lines[l];
+  for (let l = 0; l < lines.length; l++) {
+    const line = lines[l];
     tategaki(ctx, line, x - fontsize * l, y, fontsize, font);
   }
 }
 
 var fab;
-var w = 420 + 20 * 2;
-var h = (280 + 20) * 4 + 20;
+const w = 420 + 20 * 2;
+const h = (280 + 20) * 4 + 20;
 
-var loadedFonts = {};
+const loadedFonts = {};
 
 export default {
   data() {
@@ -213,12 +213,12 @@ export default {
   },
   computed: {
     input: function () {
-      var lines = this.rawinput.split("\n");
-      var komas = [];
-      var koma = [];
-      var msg = [];
-      for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
+      const lines = this.rawinput.split("\n");
+      const komas = [];
+      let koma = [];
+      let msg = [];
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
         //空行
         if (line.trim() === "" && msg.length > 0) {
           koma.push(msg.join("\n"));
@@ -271,10 +271,10 @@ export default {
     exportImage: function () {
       fab.discardActiveObject().renderAll();
       fab.discardActiveGroup().renderAll();
-      var eimage = document.querySelector("#export");
-      var canvas = document.querySelector("#output");
-      var fabel = document.querySelector("#overlay");
-      var destCtx = eimage.getContext("2d");
+      const eimage = document.querySelector("#export");
+      const canvas = document.querySelector("#output");
+      const fabel = document.querySelector("#overlay");
+      const destCtx = eimage.getContext("2d");
       destCtx.fillStyle = "white";
       destCtx.fillRect(0, 0, w, h);
       destCtx.drawImage(canvas, 0, 0);
@@ -282,27 +282,27 @@ export default {
       window.open(eimage.toDataURL("image/png"));
     },
     redraw: function () {
-      var text = this.input;
-      var ctx = this.ctx;
+      const text = this.input;
+      const ctx = this.ctx;
       ctx.clearRect(0, 0, w, h);
 
-      for (var i = 0; i < text.length; i++) {
-        var ox = 20;
-        var oy = 20 + (280 + 20) * i;
+      for (let i = 0; i < text.length; i++) {
+        const ox = 20;
+        const oy = 20 + (280 + 20) * i;
         ctx.strokeStyle = "black";
         ctx.lineWidth = 4;
         ctx.strokeRect(ox, oy, 420, 280);
 
-        var fontsize = 18;
-        var x;
-        var y = oy + 30;
+        const fontsize = 18;
+        let x;
+        const y = oy + 30;
         if (text[i]) {
           if (text[i].length > 0) {
             x = ox + 400 - fontsize;
             drawTextLines(ctx, text[i][0], x, y, fontsize, this.font);
           }
           if (text[i].length > 1) {
-            var linelength = text[i][1].split("\n").length;
+            const linelength = text[i][1].split("\n").length;
             x = ox + 20 + fontsize * (linelength - 1);
             drawTextLines(ctx, text[i][1], x, y, fontsize, this.font);
           }
@@ -315,16 +315,16 @@ export default {
       this.redraw();
     },
     font: function (item) {
-      var self = this;
-      var fontName = item;
+      const self = this;
+      const fontName = item;
 
       if (!loadedFonts[item]) {
-        var fonts = {
+        const fonts = {
           yasashisa: "url(/assets/07YasashisaAntique.otf)",
           "noto-tc": "url(/assets/NotoSerifTC-Regular.otf)",
         };
 
-        var font = new FontFace(item, fonts[item], {});
+        const font = new FontFace(item, fonts[item], {});
         font.load().then((loadedFace) => {
           setTimeout(() => {
             document.fonts.add(loadedFace);
@@ -337,17 +337,16 @@ export default {
       setTimeout(() => {
         self.redraw();
       }, 1000);
-      //var font = new FontFace("newfont", "url(07YasashisaAntique.otf)", {});
     },
   },
   mounted: function () {
-    var eimage = document.querySelector("#export");
+    const eimage = document.querySelector("#export");
     eimage.width = `${w}`;
     eimage.height = `${h}`;
     eimage.style.width = `${w}px`;
     eimage.style.height = `${h}px`;
 
-    var canvas = document.querySelector("#output");
+    const canvas = document.querySelector("#output");
 
     canvas.width = `${w}`;
     canvas.height = `${h}`;
@@ -355,7 +354,7 @@ export default {
     canvas.style.height = `${h}px`;
     this.ctx = canvas.getContext("2d");
 
-    var fabel = document.querySelector("#overlay");
+    const fabel = document.querySelector("#overlay");
     fabel.width = `${w}`;
     fabel.height = `${h}`;
     fabel.style.width = `${w}px`;
@@ -377,12 +376,12 @@ export default {
     });
 
     document.getElementById("imgLoader").onchange = function handleImage(e) {
-      var reader = new FileReader();
+      const reader = new FileReader();
       reader.onload = function (event) {
-        var imgObj = new Image();
+        const imgObj = new Image();
         imgObj.src = event.target.result;
         imgObj.onload = function () {
-          var image = new fabric.Image(imgObj);
+          const image = new fabric.Image(imgObj);
           image.set({
             height: 200,
             width: 200,
